fed8caf9d89d0cae12c0bc42086fcc86fe27076d
abjeet16 <avjeetyadav00@gmail.com>
2024-01-21 15:47:21 +0530

    completed app

--- app/src/main/java/com/example/notepadapp/MainActivity.java
+++ app/src/main/java/com/example/notepadapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.notepadapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
